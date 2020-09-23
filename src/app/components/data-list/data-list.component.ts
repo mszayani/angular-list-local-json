45cfd7bca89db2c83797322c0581a987e2b074a2
mszayani <mszayani@outlook.com>
2020-09-23 17:53:01 +0200

    adding cars list data binding & some css

--- src/app/components/data-list/data-list.component.ts
+++ src/app/components/data-list/data-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import cars from '../../../assets/data/cars.json';
 @Component({
   selector: 'app-data-list',
   templateUrl: './data-list.component.html',
   styleUrls: ['./data-list.component.css']
 })
 export class DataListComponent implements OnInit {
-
+  title="Angular Cars List";
+  public carList: {brand: string, model: string}[] =cars;
   constructor() { }
 
   ngOnInit(): void {
